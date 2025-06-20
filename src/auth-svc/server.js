87718cf9e63bf37c70f0e9b13ff164300093b23c
Oleksii Bondar <63553329+oleksiijko@users.noreply.github.com>
2025-06-20 21:58:35 +0200

    fix

--- src/auth-svc/server.js
+++ src/auth-svc/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const http     = require('http');
-const winston  = require('winston');
 const auth     = require('./authMiddleware');
 
 const app = express();
@@ -10,14 +9,10 @@ app.use(auth());
 app.get('/health', (req, res)  => res.send('healthy'));
 app.get('/secure', (req, res)  => res.json({ ok: true }));
 
-// простой консольный логгер
-const logger = winston.createLogger({
-  transports: [new winston.transports.Console()],
-});
 
 if (require.main === module) {
   http.createServer(app).listen(3000, () =>
-    logger.info('auth-svc HTTP listening on 3000')
+    console.log('auth-svc HTTP listening on 3000')
   );
 }
 
